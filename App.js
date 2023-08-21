89284a733b33224c52ab97e765ac081716de3554
italo gonçalves dos santos <italogs07@gmail.com>
2023-08-21 19:50:34 -0300

    commit inicial

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView } from 'react-native-web';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={{ marginTop: 30 }}>
+      <View style={styles.card}>
+        <Text style={styles.titulo}>Hello world</Text>
+        <Text style={styles.paragrafo}>Isto é um parágrafo.</Text>
+        <Text>Isto é um parágrafo.</Text>
+        <Text style={styles.paragrafo}>Isto é um parágrafo.</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ScrollView >
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +24,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+  card: {
+    borderColor: 'blue',
+    borderStyle: 'solid',
+    borderwidth: 2,
+  },
+
+  titulo: {
+    color: 'red',
+    fontSize: 30,
+    padding: 10,
+    marginBotton: 20,
+    borderBottonColor: 'red',
+    borderBottonStyle: 'solid',
+    borderBottonWidth: 2,
+    textAlign: 'center',
+    backgroundColor: 'red',
+  },
+  paragrafo: {
+    fontSize: 20
+  
 });
